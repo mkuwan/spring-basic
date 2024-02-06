1f52d90c1655b91e853d7725f3f0c7b64fe810a1
mkuwan <mkuwan@msn.com>
2024-02-06 23:15:27 +0900

    create-react-app

--- front-app/src/App.tsx
+++ front-app/src/App.tsx
@@ -10,7 +10,6 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <p>Hello my first application.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
