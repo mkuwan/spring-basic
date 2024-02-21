ab5822c904ce92137a3d193f2dbcd30b541fef32
mkuwan <mkuwan@msn.com>
2024-02-21 13:38:13 +0900

    CalcServiceSampleを追加

---
+++ src/main/java/org/example/springbasic/sample/CalcServiceSample.java
@@ -0,0 +1,22 @@
+package org.example.springbasic.sample;
+
+import jdk.jshell.spi.ExecutionControl;
+
+public class CalcServiceSample {
+
+    public int add(int a, int b) throws Exception {
+        throw new ExecutionControl.NotImplementedException("未実装");
+    }
+
+    public int subtract(int a, int b) throws Exception {
+        throw new ExecutionControl.NotImplementedException("未実装");
+    }
+
+    public int multiply(int a, int b) throws Exception {
+        throw new ExecutionControl.NotImplementedException("未実装");
+    }
+
+    public int divide(int a, int b) throws Exception {
+        throw new ExecutionControl.NotImplementedException("未実装");
+    }
+}
