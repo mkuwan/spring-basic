115e5cefc7d7ca444462bbffb28a0b34e8e89495
mkuwan <mkuwan@msn.com>
2024-02-28 15:59:54 +0900

    CalcServiceSampleを追加

--- src/main/java/org/example/springbasic/sample/CalcServiceSample.java
+++ src/main/java/org/example/springbasic/sample/CalcServiceSample.java
@@ -4,20 +4,81 @@ import jdk.jshell.spi.ExecutionControl;
 
 public class CalcServiceSample {
 
+    /**
+     * 足し算
+     * @param a
+     * @param b
+     * @return
+     */
     public int add(int a, int b)  {
-//        throw new ExecutionControl.NotImplementedException("未実装");
         return a + b;
     }
 
-    public int subtract(int a, int b) throws Exception {
-        throw new ExecutionControl.NotImplementedException("未実装");
+    /**
+     * 引き算
+     * @param a
+     * @param b
+     * @return
+     */
+    public int subtract(int a, int b)  {
+        return a- b;
     }
 
-    public int multiply(int a, int b) throws Exception {
-        throw new ExecutionControl.NotImplementedException("未実装");
+    /**
+     * とにかくとっても難しい計算をするメソッドです
+     *  a=1, b=5 の時は色々計算して1200が返ってきます
+     * @param a
+     * @param b
+     * @return
+     */
+    public int anyCalc(int a, int b){
+        return 0;
     }
 
-    public int divide(int a, int b) throws Exception {
-        throw new ExecutionControl.NotImplementedException("未実装");
+    /**
+     * 掛け算
+     * @param a
+     * @param b
+     * @return
+     */
+    public int multiply(int a, int b)  {
+        return a * b;
     }
+
+    /**
+     * 割り算
+     * @param a
+     * @param b
+     * @return
+     */
+    public int divide (int a, int b) {
+
+        if(b == 0){
+            throw new ArithmeticException("0で割ることはできません");
+        }
+
+        return a / b;
+
+    }
+
+
+    public int troubleCalc(int a, int b) throws Exception {
+
+        if (a == 0){
+            throw new IllegalArgumentException("aは0であってはいけません");
+        }
+
+        if (b == 0){
+            throw new ArithmeticException("bは0であってはいけません");
+        }
+
+        if (a + b > 100) {
+            throw new Exception("a + b は100を超えてはいけません");
+        }
+
+        return (a * b) / b;
+    }
+
+
+
 }
--- src/test/java/org/example/springbasic/sample/CalcServiceSampleTest.java
+++ src/test/java/org/example/springbasic/sample/CalcServiceSampleTest.java
@@ -1,5 +1,6 @@
 package org.example.springbasic.sample;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -9,10 +10,24 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class CalcServiceSampleTest {
+
+    @Test
+    void add_success(){
+        // arrange
+        CalcServiceSample calcServiceSample = new CalcServiceSample();
+
+        // act
+        int actual = calcServiceSample.add(1, 2);
+        int actual2 = calcServiceSample.add(0, 0);
+
+        // assertion
+        assertEquals(3, actual);
+        assertEquals(0, actual2);
+    }
 
     /**
      * 正常系
@@ -31,48 +46,147 @@ class CalcServiceSampleTest {
         // arrange
         CalcServiceSample calcServiceSample = new CalcServiceSample();
 
-        // act & assertion
-        assertEquals(expected, calcServiceSample.add(a, b));
-    }
+        // act
+        int actual = calcServiceSample.add(a, b);
 
-    @Test
-    void add_with_override() {
-        // arrange
-        CalcServiceSample calcServiceSample = new CalcServiceSample() {
-            @Override
-            public int add(int a, int b) {
-                return 100;
-            }
-        };
-
-        // act & assertion
-        assertEquals(100, calcServiceSample.add(1, 2));
+        // assertion
+        assertEquals(expected, actual);
     }
 
 
+
+//    @Mock
+//    CalcServiceSample calcServiceSampleMock;
+//
+//    @Test
+//    void add_with_mock(){
+//        // arrange
+//        when(calcServiceSampleMock.add(1, 2)).thenReturn(100);
+//        when(calcServiceSampleMock.add(100, 20)).thenReturn(50);
+//
+//        // act
+//        var resultA = calcServiceSampleMock.add(1, 2);
+//        var resultB = calcServiceSampleMock.add(100, 20);
+//
+//        // assertion
+//        assertEquals(100,resultA);
+//        assertEquals(50, resultB);
+//    }
+
+    /**
+     * a=1, b=5 の時は色々計算して1200が返ってきます
+     * a=10, b=8 の時は色々計算して100が返ってきます
+     * addメソッドは1回呼ばれます
+     */
     @Mock
     CalcServiceSample calcServiceSampleMock;
+    @Test
+    void anyCalc(){
+        //arrange
+        when(calcServiceSampleMock.anyCalc(1,5)).thenReturn(1200);
+        when(calcServiceSampleMock.add(10,8)).thenReturn(100);
+
+        //act
+        var resultA = calcServiceSampleMock.anyCalc(1,5);
+        var resultB = calcServiceSampleMock.add(10,8);
+
+        //assertion
+        assertEquals(1200,resultA);
+        assertEquals(100,resultB);;
+
+        // 呼ばれることを検証
+        verify( calcServiceSampleMock, times(1)).add(10,8);
+
+        // 1回呼ばれることを検証
+        verify( calcServiceSampleMock, times(1)).anyCalc(1,5);
+    }
 
     @Test
-    void add_with_mock(){
+    void subtract_success_by_nakano() {
         // arrange
-        when(calcServiceSampleMock.add(1, 2)).thenReturn(100);
-        when(calcServiceSampleMock.add(100, 20)).thenReturn(50);
+        CalcServiceSample calcServiceSample = new CalcServiceSample();
 
-        // act & assertion
-        assertEquals(100, calcServiceSampleMock.add(1, 2));
-        assertEquals(50, calcServiceSampleMock.add(100, 20));
+        //act
+        int result = calcServiceSample.subtract(1,1);
+        // assertion
+        assertEquals(0,result);
     }
 
-    @Test
-    void subtract() {
+    @ParameterizedTest
+    @CsvSource({"1,2,-1","0,0,0","8,5,3"})
+    void subtract_success_by_nakanoParam(int a,int b,int expected) {
+        //arrange
+        CalcServiceSample calcServiceSample = new CalcServiceSample();
+
+        //act
+        int result = calcServiceSample.subtract(a,b);
+
+        //assertion
+        assertEquals(expected,result);
     }
 
-    @Test
-    void multiply() {
-    }
+
+
 
     @Test
     void divide() {
+    }
+
+    @Test
+    void divide_assertThrow(){
+        // arrange
+        CalcServiceSample calcServiceSample = new CalcServiceSample();
+
+        // act
+        var ex = assertThrows(ArithmeticException.class, ()-> calcServiceSample.divide(3,0));
+
+        // assertion
+//        assertEquals("例外が発生しました", ex.getMessage());
+
+    }
+
+
+//    public int troubleCalc(int a, int b) throws Exception {
+//
+//        if (a == 0){
+//            throw new IllegalArgumentException("aは0であってはいけません");
+//        }
+//
+//        if (b == 0){
+//            throw new ArithmeticException("bは0であってはいけません");
+//        }
+//
+//        if (a + b > 100) {
+//            throw new Exception("a + b は100を超えてはいけません");
+//        }
+//
+//        return (a * b) / b;
+//    }
+
+
+    /**
+     * a=0 の時はIllegalArgumentExceptionが発生します
+     */
+    @Test
+    void troubleCalcTestA(){
+        // arrange
+        CalcServiceSample calcServiceSample = new CalcServiceSample();
+
+        //act & assertion
+        var ex = assertThrows(IllegalArgumentException.class,() -> calcServiceSample.troubleCalc(0,2));
+        assertEquals("aは0であってはいけません",ex.getMessage());
+    }
+
+    /**
+     * b=0 の時はArithmeticExceptionが発生します
+     */
+    @DisplayName("b=0 の時のテスト")
+    @Test
+    void troubleCalcTestB(){
+        CalcServiceSample calcServiceSample = new CalcServiceSample();
+
+        var ex = assertThrows(ArithmeticException.class,() -> calcServiceSample.troubleCalc(2,0));
+         assertEquals("bは0であってはいけません",ex.getMessage());
+
     }
 }
